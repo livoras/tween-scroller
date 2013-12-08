4f3d9bc10b2578b0442a6849a419bbc041597044
khhhshhh <iammfw@163.com>
2013-12-08 17:05:17 +0800

    Add Scrolling Ability

--- bin/tween-scroller.js
+++ bin/tween-scroller.js
@@ -1,25 +1,62 @@
 (function() {
   $.fn.scroll = function(options) {
-    var addScrollbar, dragAndDropScrollBar, exportAPIs, getPace, listenMouseWheelEvent, scroll, toggleShowHideScrollBarOnHover,
+    var accelerator, addScrollbar, coefficienceK, detailTime, dragAndDropScrollBar, exportAPIs, getAccelerator, getVelocity, listenMouseWheelEvent, mass, maxVelocity, minVelocity, scroll, toggleShowHideScrollBarOnHover,
       _this = this;
+    detailTime = 1000 / 60;
+    mass = 1;
+    maxVelocity = 60;
+    minVelocity = 0.8;
+    accelerator = 4;
+    coefficienceK = 0.97;
     listenMouseWheelEvent = function($outer) {
       return $outer.on('mousewheel DOMMouseScroll', $.proxy(scroll, $outer));
     };
     scroll = function(event) {
-      var $outer, pace;
+      var $inner, $outer, scrolling, velocity;
+      event.preventDefault();
       $outer = this;
-      pace = getPace(event);
-      $outer.$inner.css('top', -pace * 10 + 'px');
-      return $outer.$scrollbar;
+      $inner = $outer.$inner;
+      velocity = getVelocity($outer.velocity, getAccelerator(event));
+      $outer.velocity = velocity;
+      scrolling = function(event) {
+        clearTimeout($outer.scrollTimer);
+        if (Math.abs(velocity) < minVelocity) {
+          $outer.velocity = 0;
+          return;
+        }
+        $inner.css('top', "+=" + velocity);
+        velocity = getVelocity(velocity, 0);
+        $outer.velocity = velocity;
+        return $outer.scrollTimer = setTimeout(function() {
+          return scrolling();
+        }, detailTime);
+      };
+      return scrolling(event);
     };
-    getPace = function(event) {
-      var scrollTo;
+    getAccelerator = function(event) {
+      var sign;
       if (event.type === 'mousewheel') {
-        scrollTo = event.originalEvent.wheelDelta / 120 * -1;
+        sign = event.originalEvent.wheelDelta / 120;
       } else if (event.type === 'DOMMouseScroll') {
-        scrollTo = event.originalEvent.detail / 3;
+        sign = event.originalEvent.detail;
       }
-      return scrollTo;
+      return sign * accelerator;
+    };
+    getVelocity = function(velocity, accelerator) {
+      var isReverse, sign;
+      isReverse = velocity * accelerator < 0 ? true : false;
+      if (isReverse) {
+        velocity = accelerator;
+      } else {
+        if (accelerator === 0) {
+          velocity = velocity * coefficienceK;
+        } else {
+          velocity += accelerator;
+        }
+        sign = velocity < 0 ? -1 : 1;
+        velocity = Math.abs(velocity) > maxVelocity ? sign * maxVelocity : velocity;
+      }
+      return velocity;
     };
     addScrollbar = function($outer) {
       var $scrollbar;
@@ -42,6 +79,7 @@
       $inner = $outer.find('div.scroll-inner:eq(0)');
       $outer.$inner = $inner;
       addScrollbar($outer);
+      $outer.velocity = 0;
       listenMouseWheelEvent($outer);
       dragAndDropScrollBar($outer);
       toggleShowHideScrollBarOnHover($outer);
--- src/tween-scroller.coffee
+++ src/tween-scroller.coffee
@@ -1,20 +1,56 @@
 $.fn.scroll = (options)->
+  detailTime = 1000 / 60 # frame
+  mass = 1
+
+  maxVelocity = 60
+  minVelocity = 0.8
+  
+  accelerator = 4
+  coefficienceK = 0.97
 
   listenMouseWheelEvent = ($outer)->
     $outer.on 'mousewheel DOMMouseScroll', $.proxy scroll, $outer
 
   scroll = (event)->
+    event.preventDefault()
     $outer = @
-    pace = getPace event
-    $outer.$inner.css 'top', -pace * 10 + 'px'
-    $outer.$scrollbar
+    $inner = $outer.$inner
+    velocity = getVelocity $outer.velocity, getAccelerator event
+    $outer.velocity = velocity
 
-  getPace = (event)=>  
-    if event.type is 'mousewheel'
-      scrollTo = event.originalEvent.wheelDelta / 120 * -1
-    else if event.type is'DOMMouseScroll'
-      scrollTo = event.originalEvent.detail / 3
-    scrollTo  
+    scrolling = (event)->
+      clearTimeout $outer.scrollTimer
+      if Math.abs(velocity) < minVelocity
+        $outer.velocity = 0
+        return 
+      $inner.css 'top', "+=#{velocity}"
+      velocity = getVelocity velocity, 0
+      $outer.velocity = velocity
+      $outer.scrollTimer = setTimeout ->
+        scrolling()
+      , detailTime   
+
+    scrolling(event)  
+
+  getAccelerator = (event)=>
+    if event.type is 'mousewheel' # For Chrome
+      sign = event.originalEvent.wheelDelta / 120 
+    else if event.type is 'DOMMouseScroll' # For Firefox
+      sign = event.originalEvent.detail
+    sign * accelerator  
+
+  getVelocity = (velocity, accelerator)=>  
+    isReverse = if velocity * accelerator < 0 then yes else no
+    if isReverse
+      velocity = accelerator
+    else 
+      if accelerator is 0
+        velocity = velocity * coefficienceK
+      else 
+        velocity += accelerator
+      sign = if velocity < 0 then -1 else 1
+      velocity = if Math.abs(velocity) > maxVelocity then sign * maxVelocity else velocity
+    velocity
 
   addScrollbar = ($outer)->    
     $scrollbar = $ "<div class='scrollbar'></div>"
@@ -40,8 +76,10 @@ $.fn.scroll = (options)->
     $inner = $outer.find 'div.scroll-inner:eq(0)'
     $outer.$inner = $inner
     addScrollbar $outer
+
+    $outer.velocity = 0
+
     listenMouseWheelEvent $outer
     dragAndDropScrollBar $outer
     toggleShowHideScrollBarOnHover $outer
     exportAPIs $outer
-

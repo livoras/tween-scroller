ce7a2bfb7dfe83666eb4cff332df23deb243d818
khhhshhh <iammfw@163.com>
2013-12-09 21:38:20 +0800

    almost finish

--- bin/tween-scroller.js
+++ bin/tween-scroller.js
@@ -1,6 +1,6 @@
 (function() {
   $.fn.scroll = function(options) {
-    var accelerator, addScrollbar, coefficienceK, detailTime, dragAndDropScrollBar, exportAPIs, getAccelerator, getVelocity, listenMouseWheelEvent, mass, maxVelocity, minVelocity, scroll, toggleShowHideScrollBarOnHover,
+    var $document, EXCEED_DOWN, EXCEED_UP, NO_EXCEED, accelerator, addScrollbar, allowExceedLength, backTime, coefficienceK, detailTime, dragAndDropScrollBar, exceedCoefficience, exportAPIs, getAccelerator, getExceedCoefficience, getNextVelocity, hideScrollbar, isStop, listenMouseWheelEvent, mass, maxVelocity, minVelocity, preventScrollbarExceed, resetScrollbarHeight, resetScrollbarPosition, scroll, scrollBack, scrollInnerCoordinateWithScrollBar, scrollbarMinHeight, scrolling, scrollingScrollbar, showScrollbar, stopScrolling, toggleShowHideScrollBarOnHover, _ref,
       _this = this;
     detailTime = 1000 / 60;
     mass = 1;
@@ -8,30 +8,123 @@
     minVelocity = 0.8;
     accelerator = 4;
     coefficienceK = 0.97;
+    exceedCoefficience = 0.7;
+    allowExceedLength = 50;
+    backTime = 500;
+    scrollbarMinHeight = 50;
+    _ref = [0, 1, 2], EXCEED_UP = _ref[0], EXCEED_DOWN = _ref[1], NO_EXCEED = _ref[2];
+    $document = $(document);
     listenMouseWheelEvent = function($outer) {
       return $outer.on('mousewheel DOMMouseScroll', $.proxy(scroll, $outer));
     };
     scroll = function(event) {
-      var $inner, $outer, scrolling, velocity;
+      var $inner, $outer, $scrollbar, velocity;
       event.preventDefault();
       $outer = this;
       $inner = $outer.$inner;
-      velocity = getVelocity($outer.velocity, getAccelerator(event));
-      $outer.velocity = velocity;
-      scrolling = function(event) {
-        clearTimeout($outer.scrollTimer);
-        if (Math.abs(velocity) < minVelocity) {
-          $outer.velocity = 0;
-          return;
+      $scrollbar = $outer.$scrollbar;
+      resetScrollbarHeight($outer);
+      velocity = getNextVelocity($outer.velocity, getAccelerator(event));
+      scrolling($outer, velocity);
+      scrollingScrollbar($outer);
+      return $scrollbar.stop(true, true);
+    };
+    scrolling = function($outer, velocity) {
+      var $inner, $scrollbar, addOnCoefficience;
+      clearTimeout($outer.scrollTimer);
+      $inner = $outer.$inner;
+      $scrollbar = $outer.$scrollbar;
+      $inner.stop(true, false);
+      $inner.css('top', "+=" + velocity);
+      $outer.scrollTimer = setTimeout(function() {
+        return scrolling($outer, velocity);
+      }, detailTime);
+      if (isStop(velocity)) {
+        stopScrolling($outer);
+        if ($outer.isExceed) {
+          scrollBack($outer);
         }
-        $inner.css('top', "+=" + velocity);
-        velocity = getVelocity(velocity, 0);
-        $outer.velocity = velocity;
-        return $outer.scrollTimer = setTimeout(function() {
-          return scrolling();
-        }, detailTime);
-      };
-      return scrolling(event);
+        return;
+      }
+      addOnCoefficience = getExceedCoefficience($outer);
+      velocity = getNextVelocity(velocity, 0, addOnCoefficience);
+      return $outer.velocity = velocity;
+    };
+    isStop = function(velocity) {
+      return Math.abs(velocity) < minVelocity;
+    };
+    getExceedCoefficience = function($outer) {
+      var $inner, innerTop, maxInnerTop, minInnerTop;
+      $inner = $outer.$inner;
+      innerTop = $inner.position().top;
+      minInnerTop = $outer.outerHeight() - $inner.outerHeight();
+      maxInnerTop = 0;
+      if (innerTop > maxInnerTop) {
+        $outer.isExceed = true;
+        $outer.exceedDistance = innerTop;
+        return exceedCoefficience;
+      } else if (innerTop < minInnerTop) {
+        $outer.isExceed = true;
+        $outer.exceedDistance = innerTop - minInnerTop;
+        return exceedCoefficience;
+      } else {
+        $outer.isExceed = false;
+        return 1;
+      }
+    };
+    scrollBack = function($outer) {
+      var $inner, $scrollbar, distance, minInnerTop, minScrollbarTop;
+      $inner = $outer.$inner;
+      $scrollbar = $outer.$scrollbar;
+      distance = $outer.exceedDistance;
+      minInnerTop = $outer.outerHeight() - $inner.outerHeight();
+      minScrollbarTop = $outer.outerHeight() - $scrollbar.outerHeight();
+      if (distance < 0) {
+        $inner.stop(true, false).animate({
+          'top': minInnerTop
+        }, backTime);
+        return $scrollbar.stop().animate({
+          'top': minScrollbarTop
+        }, backTime);
+      } else {
+        $inner.stop(true, false).animate({
+          'top': 0
+        }, backTime);
+        return $scrollbar.stop().animate({
+          'top': 0
+        }, backTime);
+      }
+    };
+    stopScrolling = function($outer) {
+      $outer.velocity = 0;
+      clearTimeout($outer.scrollbarTimer);
+      return clearTimeout($outer.scrollTimer);
+    };
+    scrollingScrollbar = function($outer) {
+      clearTimeout($outer.scrollbarTimer);
+      resetScrollbarPosition($outer);
+      return $outer.scrollbarTimer = setTimeout(function() {
+        return scrollingScrollbar($outer);
+      }, detailTime);
+    };
+    resetScrollbarHeight = function($outer) {
+      var innerHeight, outerHeight, scrollbarHeight;
+      outerHeight = $outer.outerHeight();
+      innerHeight = $outer.$inner.outerHeight();
+      scrollbarHeight = outerHeight * (outerHeight - scrollbarMinHeight) / innerHeight + scrollbarMinHeight;
+      return $outer.$scrollbar.animate({
+        'height': scrollbarHeight
+      }, 300);
+    };
+    resetScrollbarPosition = function($outer) {
+      var $inner, $scrollbar, innerHeight, innerTop, outerHeight, scrollbarHeight, scrollbarTop, _ref1;
+      _ref1 = [$outer.$scrollbar, $outer.$inner], $scrollbar = _ref1[0], $inner = _ref1[1];
+      innerTop = $inner.position().top;
+      outerHeight = $outer.outerHeight();
+      innerHeight = $inner.outerHeight();
+      scrollbarHeight = $scrollbar.outerHeight();
+      scrollbarTop = -innerTop * (outerHeight - scrollbarHeight) / (innerHeight - outerHeight);
+      return $scrollbar.css('top', scrollbarTop);
     };
     getAccelerator = function(event) {
       var sign;
@@ -42,14 +135,17 @@
       }
       return sign * accelerator;
     };
-    getVelocity = function(velocity, accelerator) {
+    getNextVelocity = function(velocity, accelerator, addOnCoefficience) {
       var isReverse, sign;
+      if (addOnCoefficience == null) {
+        addOnCoefficience = 1;
+      }
       isReverse = velocity * accelerator < 0 ? true : false;
       if (isReverse) {
         velocity = accelerator;
       } else {
         if (accelerator === 0) {
-          velocity = velocity * coefficienceK;
+          velocity = velocity * coefficienceK * addOnCoefficience;
         } else {
           velocity += accelerator;
         }
@@ -64,13 +160,80 @@
       $outer.$scrollbar = $scrollbar;
       return $outer.append($scrollbar);
     };
-    dragAndDropScrollBar = function($outer) {};
+    dragAndDropScrollBar = function($outer) {
+      var $scrollbar, drag, drop, move, preClientY, scrollbarTop;
+      $scrollbar = $outer.$scrollbar;
+      scrollbarTop = 0;
+      preClientY = 0;
+      drag = function(event) {
+        event.preventDefault();
+        $outer.isDragging = true;
+        stopScrolling($outer);
+        scrollbarTop = $scrollbar.position().top;
+        preClientY = event.clientY;
+        return $document.on('mousemove', move);
+      };
+      drop = function(event) {
+        event.preventDefault();
+        $outer.isDragging = false;
+        if (!$outer.isMouseover) {
+          hideScrollbar($outer);
+        }
+        return $document.off('mousemove', move);
+      };
+      move = function(event) {
+        event.preventDefault();
+        scrollbarTop = event.clientY - preClientY + $scrollbar.position().top;
+        scrollbarTop = preventScrollbarExceed(scrollbarTop, $outer);
+        preClientY = event.clientY;
+        $scrollbar.css('top', scrollbarTop);
+        return scrollInnerCoordinateWithScrollBar($outer);
+      };
+      $scrollbar.on('mousedown', drag);
+      return $document.on('mouseup', drop);
+    };
+    preventScrollbarExceed = function(scrollbarTop, $outer) {
+      var $scrollbar, maxScrollbarTop;
+      $scrollbar = $outer.$scrollbar;
+      maxScrollbarTop = $outer.outerHeight() - $scrollbar.outerHeight();
+      if (scrollbarTop > maxScrollbarTop) {
+        scrollbarTop = maxScrollbarTop;
+      }
+      if (scrollbarTop < 0) {
+        scrollbarTop = 0;
+      }
+      return scrollbarTop;
+    };
+    scrollInnerCoordinateWithScrollBar = function($outer) {
+      var $inner, $scrollbar, innerHeight, innerTop, outerHeight, scrollbarHeight, scrollbarTop, _ref1;
+      _ref1 = [$outer.$scrollbar, $outer.$inner], $scrollbar = _ref1[0], $inner = _ref1[1];
+      innerHeight = $inner.outerHeight();
+      outerHeight = $outer.outerHeight();
+      scrollbarHeight = $scrollbar.outerHeight();
+      scrollbarTop = $scrollbar.position().top;
+      innerTop = -scrollbarTop * (innerHeight - outerHeight) / (outerHeight - scrollbarHeight);
+      return $inner.css('top', innerTop);
+    };
     toggleShowHideScrollBarOnHover = function($outer) {
       return $outer.hover(function() {
-        return $outer.$scrollbar.stop(true, true).fadeIn(200);
+        $outer.isMouseover = true;
+        return showScrollbar($outer);
       }, function() {
-        return $outer.$scrollbar.stop(true, true).fadeOut(200);
+        $outer.isMouseover = false;
+        return hideScrollbar($outer);
       });
+    };
+    showScrollbar = function($outer) {
+      if ($outer.isDragging) {
+        return;
+      }
+      return $outer.$scrollbar.stop(true, true).fadeIn(200);
+    };
+    hideScrollbar = function($outer) {
+      if ($outer.isDragging) {
+        return;
+      }
+      return $outer.$scrollbar.stop(true, true).fadeOut(200);
     };
     exportAPIs = function($outer) {};
     return this.each(function(i, elem) {
@@ -79,6 +242,7 @@
       $inner = $outer.find('div.scroll-inner:eq(0)');
       $outer.$inner = $inner;
       addScrollbar($outer);
+      resetScrollbarHeight($outer);
       $outer.velocity = 0;
       listenMouseWheelEvent($outer);
       dragAndDropScrollBar($outer);
--- src/tween-scroller.coffee
+++ src/tween-scroller.coffee
@@ -7,6 +7,15 @@ $.fn.scroll = (options)->
   
   accelerator = 4
   coefficienceK = 0.97
+  exceedCoefficience = 0.7
+
+  allowExceedLength = 50
+  backTime = 500
+
+  scrollbarMinHeight = 50
+  [EXCEED_UP, EXCEED_DOWN, NO_EXCEED] = [0, 1, 2] 
+
+  $document = $ document
 
   listenMouseWheelEvent = ($outer)->
     $outer.on 'mousewheel DOMMouseScroll', $.proxy scroll, $outer
@@ -15,22 +24,98 @@ $.fn.scroll = (options)->
     event.preventDefault()
     $outer = @
     $inner = $outer.$inner
-    velocity = getVelocity $outer.velocity, getAccelerator event
+    $scrollbar = $outer.$scrollbar
+
+    resetScrollbarHeight $outer
+
+    velocity = getNextVelocity $outer.velocity, getAccelerator event
+    scrolling $outer, velocity
+    scrollingScrollbar $outer
+    $scrollbar.stop(true, true)
+
+  scrolling = ($outer, velocity)->
+    clearTimeout $outer.scrollTimer
+    $inner = $outer.$inner
+    $scrollbar = $outer.$scrollbar
+
+    $inner.stop(true, false)
+    $inner.css 'top', "+=#{velocity}"
+    $outer.scrollTimer = setTimeout ->
+      scrolling $outer, velocity
+    , detailTime   
+
+    if isStop velocity
+      stopScrolling $outer
+      if $outer.isExceed then scrollBack $outer
+      return 
+
+    addOnCoefficience = getExceedCoefficience $outer
+    velocity = getNextVelocity velocity, 0, addOnCoefficience
     $outer.velocity = velocity
 
-    scrolling = (event)->
-      clearTimeout $outer.scrollTimer
-      if Math.abs(velocity) < minVelocity
-        $outer.velocity = 0
-        return 
-      $inner.css 'top', "+=#{velocity}"
-      velocity = getVelocity velocity, 0
-      $outer.velocity = velocity
-      $outer.scrollTimer = setTimeout ->
-        scrolling()
-      , detailTime   
+  isStop = (velocity)->  
+    Math.abs(velocity) < minVelocity
 
-    scrolling(event)  
+  getExceedCoefficience = ($outer)=>  
+    $inner = $outer.$inner
+    innerTop = $inner.position().top
+    minInnerTop = $outer.outerHeight() - $inner.outerHeight()
+    maxInnerTop = 0
+    if innerTop > maxInnerTop 
+      $outer.isExceed = true
+      $outer.exceedDistance = innerTop 
+      return exceedCoefficience
+    else if innerTop < minInnerTop
+      $outer.isExceed = true
+      $outer.exceedDistance = innerTop - minInnerTop
+      return exceedCoefficience
+    else
+      $outer.isExceed = false
+      return 1
+
+  scrollBack = ($outer)=>
+    $inner = $outer.$inner
+    $scrollbar = $outer.$scrollbar
+    distance = $outer.exceedDistance
+    minInnerTop = $outer.outerHeight() - $inner.outerHeight()
+    minScrollbarTop = $outer.outerHeight() - $scrollbar.outerHeight()
+    if distance < 0
+      $inner.stop(true, false).animate 'top': minInnerTop, backTime
+      $scrollbar.stop().animate 'top':minScrollbarTop, backTime
+    else 
+      $inner.stop(true, false).animate 'top': 0, backTime
+      $scrollbar.stop().animate 'top': 0, backTime
+
+  stopScrolling = ($outer)=>
+    $outer.velocity = 0
+    clearTimeout $outer.scrollbarTimer
+    clearTimeout $outer.scrollTimer
+
+  # The reason here seperating two animation timer for scrolling inner and scrolling scrollbar is   
+  # that, if we wire scrolling inner and scrolling bar in the same timer will cause disgusting animation
+  # delay, DOM accessing for scrolling scrollbar will speed down the animation. 
+  scrollingScrollbar = ($outer)=>
+    clearTimeout $outer.scrollbarTimer
+    resetScrollbarPosition $outer
+    $outer.scrollbarTimer = setTimeout ->
+      scrollingScrollbar $outer
+    , detailTime
+
+  resetScrollbarHeight = ($outer)=>  
+    outerHeight = $outer.outerHeight()
+    innerHeight = $outer.$inner.outerHeight()
+    scrollbarHeight = outerHeight * (outerHeight - scrollbarMinHeight) / innerHeight + scrollbarMinHeight
+    $outer.$scrollbar.animate {'height': scrollbarHeight}, 300
+
+  resetScrollbarPosition = ($outer)=>
+    [$scrollbar, $inner] = [$outer.$scrollbar, $outer.$inner]
+    innerTop = $inner.position().top
+    outerHeight = $outer.outerHeight()
+    innerHeight = $inner.outerHeight()
+    scrollbarHeight = $scrollbar.outerHeight()
+
+    scrollbarTop = -innerTop * (outerHeight - scrollbarHeight) / (innerHeight - outerHeight)
+    $scrollbar.css 'top', scrollbarTop
 
   getAccelerator = (event)=>
     if event.type is 'mousewheel' # For Chrome
@@ -39,13 +124,13 @@ $.fn.scroll = (options)->
       sign = event.originalEvent.detail
     sign * accelerator  
 
-  getVelocity = (velocity, accelerator)=>  
+  getNextVelocity = (velocity, accelerator, addOnCoefficience=1)=>  
     isReverse = if velocity * accelerator < 0 then yes else no
     if isReverse
       velocity = accelerator
     else 
       if accelerator is 0
-        velocity = velocity * coefficienceK
+        velocity = velocity * coefficienceK * addOnCoefficience
       else 
         velocity += accelerator
       sign = if velocity < 0 then -1 else 1
@@ -58,16 +143,70 @@ $.fn.scroll = (options)->
     $outer.append $scrollbar
 
   dragAndDropScrollBar = ($outer)->  
+    $scrollbar = $outer.$scrollbar
+    scrollbarTop = 0
+    preClientY = 0
+
+    drag = (event)-> 
+      event.preventDefault()
+      $outer.isDragging = true
+      stopScrolling $outer
+      scrollbarTop = $scrollbar.position().top
+      preClientY = event.clientY
+      $document.on 'mousemove', move
+
+    drop = (event)-> 
+      event.preventDefault()
+      $outer.isDragging = false
+      if not $outer.isMouseover then hideScrollbar $outer
+      $document.off 'mousemove', move
+
+    move = (event)-> 
+      event.preventDefault()
+      scrollbarTop = event.clientY - preClientY + $scrollbar.position().top
+      scrollbarTop = preventScrollbarExceed scrollbarTop, $outer
+      preClientY = event.clientY
+      $scrollbar.css 'top', scrollbarTop
+      scrollInnerCoordinateWithScrollBar $outer
+
+    $scrollbar.on 'mousedown', drag
+    $document.on 'mouseup', drop
+
+  preventScrollbarExceed = (scrollbarTop, $outer)=>
+    $scrollbar = $outer.$scrollbar
+    maxScrollbarTop = $outer.outerHeight() - $scrollbar.outerHeight()
+    if scrollbarTop > maxScrollbarTop then scrollbarTop = maxScrollbarTop 
+    if scrollbarTop < 0 then scrollbarTop = 0
+    scrollbarTop
+
+  scrollInnerCoordinateWithScrollBar = ($outer)=>
+    [$scrollbar, $inner] = [$outer.$scrollbar, $outer.$inner]
+    innerHeight = $inner.outerHeight()
+    outerHeight = $outer.outerHeight() 
+    scrollbarHeight = $scrollbar.outerHeight()
+    scrollbarTop = $scrollbar.position().top
+    innerTop = -scrollbarTop * (innerHeight - outerHeight) / (outerHeight - scrollbarHeight)
+    $inner.css 'top', innerTop
 
   toggleShowHideScrollBarOnHover = ($outer)->
     $outer.hover ->
-      $outer.$scrollbar
-            .stop(true, true)
-            .fadeIn 200
+      $outer.isMouseover = yes
+      showScrollbar $outer
     , ->  
-      $outer.$scrollbar
-            .stop(true, true)
-            .fadeOut 200
+      $outer.isMouseover = no
+      hideScrollbar $outer
+
+  showScrollbar = ($outer)->    
+    if $outer.isDragging then return 
+    $outer.$scrollbar
+          .stop(true, true)
+          .fadeIn 200
+
+  hideScrollbar = ($outer)->        
+    if $outer.isDragging then return 
+    $outer.$scrollbar
+          .stop(true, true)
+          .fadeOut 200
 
   exportAPIs = ($outer)->
 
@@ -75,7 +214,9 @@ $.fn.scroll = (options)->
     $outer = $ elem
     $inner = $outer.find 'div.scroll-inner:eq(0)'
     $outer.$inner = $inner
+
     addScrollbar $outer
+    resetScrollbarHeight $outer
 
     $outer.velocity = 0
 
